583e0b044c57f4942f7e937713b7c32ce8542e22
Adam E. Hunt <60417234+adamehunt@users.noreply.github.com>
2025-05-09 00:11:24 -0700

    Update disable-image-sizes.php

--- custom-functions/disable-image-sizes.php
+++ custom-functions/disable-image-sizes.php
@@ -1,18 +1,17 @@
-<?php 
-// disable generated image sizes
-function shapeSpace_disable_image_sizes($sizes) {
-	
-	unset($sizes['thumbnail']);    // disable thumbnail size
-	unset($sizes['medium']);       // disable medium size
-	unset($sizes['large']);        // disable large size
-	unset($sizes['medium_large']); // disable medium-large size
-	unset($sizes['1536x1536']);    // disable 2x medium-large size
-	unset($sizes['2048x2048']);    // disable 2x large size
-	
-	return $sizes;
-	
-}
-add_action('intermediate_image_sizes_advanced', 'shapeSpace_disable_image_sizes');
+<?php
+// disable generated image sizes using an anonymous function
+add_action('intermediate_image_sizes_advanced', function ($sizes) {
 
-// disable scaled image size
+	/* unset($sizes['thumbnail']); // disable thumbnail size
+	unset($sizes['medium']); // disable medium size
+	unset($sizes['large']); // disable large size */
+	unset($sizes['medium_large']); // disable medium-large size
+	unset($sizes['1536x1536']); // disable 2x medium-large size
+	unset($sizes['2048x2048']); // disable 2x large size
+
+	return $sizes;
+
+}); // <-- Note the semicolon here
+
+// disable scaled image size (this part was already using a standard WP function)
 add_filter('big_image_size_threshold', '__return_false');
